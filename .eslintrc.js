c730576025a1ca2c0a5082d20b64266ba03e21bc
OrcaXS Suen <sun16281394@gmail.com>
2017-08-15 19:16:38 +0800

    Add dot files

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
+  // add your custom rules here
+  'rules': {
+    'no-unused-vars': 'warn',
+    'no-trailing-spaces': 'warn',
+    'no-underscore-dangle': 'off',
+    'comma-dangle': ['warn', 'always-multiline'],
+    'import/prefer-default-export': 'warn',
+    'semi': ['error', 'always', { 'omitLastInOneLineBlock': true }],
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    // allow optionalDependencies
+    'import/no-extraneous-dependencies': ['error', {
+      'optionalDependencies': ['test/unit/index.js']
+    }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
