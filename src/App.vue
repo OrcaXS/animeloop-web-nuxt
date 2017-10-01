a85f82b0d7b98626a09ee46a340f9a39436b6d8d
OrcaXS Suen <sun16281394@gmail.com>
2017-10-02 00:42:40 +0800

    Use sass-loader to load Bulma

--- src/App.vue
+++ src/App.vue
@@ -88,4 +88,36 @@ body {
 }
 
 </style>
-<style src="./assets/bulma.css"></style>
+<style lang="scss">
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+// Set your colors
+$primary: #8c67ef;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099FF;
+$twitter-invert: findColorInvert($twitter);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+    "white": ($white, $black),
+    "black": ($black, $white),
+    "light": ($light, $light-invert),
+    "dark": ($dark, $dark-invert),
+    "primary": ($primary, $primary-invert),
+    "info": ($info, $info-invert),
+    "success": ($success, $success-invert),
+    "warning": ($warning, $warning-invert),
+    "danger": ($danger, $danger-invert),
+    "twitter": ($twitter, $twitter-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+</style>
+<!-- <style src="./assets/bulma.css"></style> -->
