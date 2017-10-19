89ac32403927ee608789dbc7191d18ab164e174f
OrcaXS Suen <sun16281394@gmail.com>
2017-10-20 00:02:10 +0800

    Add BreadCrumb

---
+++ src/components/partial/BreadCrumb.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      <li><a href="#">Bulma</a></li>
+      <li><a href="#">Documentation</a></li>
+      <li><a href="#">Components</a></li>
+      <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
