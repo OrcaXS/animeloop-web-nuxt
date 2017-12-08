3cb1199f0cb53e544f591b4b8f90c78415421d29
OrcaXS Suen <sun16281394@gmail.com>
2017-12-08 23:29:51 +0800

    Update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   plugins: {
-<<<<<<< HEAD
-=======
     'postcss-import': {},
     'postcss-url': {},
->>>>>>> 3695299... New branch
     'postcss-cssnext': {},
     'postcss-reporter': {},
   },
