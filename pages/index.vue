46f6e4d2f69d9cbe9379d3b7ca937fdd77d3f641
OrcaXS Suen <sun16281394@gmail.com>
2017-12-21 14:00:40 +0800

    Update Search and homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,30 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        animeloop-web-nuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <LoopGrid type="random" />
 </template>
 
 <script>
-import Logo from '~/components/Logo';
+import LoopGrid from '~/components/LoopGrid';
 
 export default {
+  name: 'Home',
   components: {
-    Logo,
+    LoopGrid,
   },
+
+  head() {
+    return {
+      title: 'Home | Animeloop',
+    };
+  },
+
+  asyncData({ store }) {
+    return store.dispatch('fetchRandomLoopList', { count: 12 });
+  },
+
 };
+
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style scoped>
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
---
+++ pages/search.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <p>SearchPage</p>
+    <input v-model="keyword" type="text" value="keyword" placeholder="Search...">
+    <button @click="dispatchSearch">Go</button>
+    <SeriesGrid type="search" />
+  </div>
+</template>
+
+<script>
+import SeriesGrid from '~/components/SeriesGrid';
+
+export default {
+  name: 'SearchPage',
+  components: {
+    SeriesGrid,
+  },
+
+  data() {
+    return {
+      hasKeyword: !!this.$route.query.keyword,
+      keyword: this.$route.query.keyword,
+    };
+  },
+
+  head() {
+    return {
+      title: this.hasKeyword ? `"${this.keyword}" | Search | Animeloop` : 'Search | Animeloop',
+    };
+  },
+
+  methods: {
+    dispatchSearch() {
+      console.log(this.keyword);
+      this.$router.push({ path: 'search', query: { keyword: this.keyword } });
+      this.$store.dispatch('fetchSeriesByString', { searchString: this.keyword });
+    },
+  },
+
+  created() {
+    if (this.hasKeyword) this.dispatchSearch();
+  },
+
+  // fetch({ store, route: { params: { keyword } } }) {
+  //   return store.dispatch('fetchSeriesByString', { searchString: keyword });
+  // },
+
+};
+</script>
+
+<style>
+
+</style>
