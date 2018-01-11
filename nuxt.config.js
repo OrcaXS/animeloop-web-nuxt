8735731209202e6bf147a816c07aecabcfa9bd40
OrcaXS Suen <sun16281394@gmail.com>
2018-01-12 00:33:16 +0800

    Add i18n, cookies, font-awesome and click-outside plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+// const VueI18n = require('vue-i18n');
+const i18nExtensions = require('vue-i18n-extensions');
+
 module.exports = {
   /*
   ** Headers of the page
@@ -7,36 +10,65 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Animeloop-nuxt' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#1E50A2' },
   /*
   ** Build configuration
   */
   build: {
+    vendor: ['vue-i18n', '@fortawesome/vue-fontawesome'],
     /*
     ** Run ESLint on save
     */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
+    extend(config, ctx) {
+      if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+          exclude: /(node_modules)/,
+        });
       }
-    }
+    },
   },
+
+  css: [
+    '@fortawesome/fontawesome/styles.css',
+    // join(__dirname, 'assets/css/mediaquery.css'),
+    // { src: 'assets/css/mediaqueryshit.css', lang: 'postcss' },
+  ],
+
+  render: {
+    // confiture `render`
+    // see Nuxt.js docs: https://nuxtjs.org/api/configuration-render#bundleRenderer
+    bundleRenderer: {
+      directives: {
+        t: i18nExtensions.directive,
+      },
+    },
+  },
+
+  plugins: [
+    '~/plugins/i18n.js',
+    '~/plugins/cookies.js',
+    '~/plugins/font-awesome.js',
+    { src: '~/plugins/v-click-outside', ssr: false },
+  ],
+
+  router: {
+    middleware: 'setLang',
+  },
+
   modules: [
     '@nuxtjs/webpackmonitor',
     'nuxt-webpackdashboard',
   ],
-}
+};
