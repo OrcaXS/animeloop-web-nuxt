d685b392fdc4a60fe1cfd72c45b7c541383ffcac
OrcaXS Suen <sun16281394@gmail.com>
2018-01-12 00:38:23 +0800

    Update breadcrumb style

--- components/BreadCrumb.vue
+++ components/BreadCrumb.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="breadcrumb-wrapper">
-    <span class="breadcrumb-link"><slot name="title"></slot></span>
+    <span class="breadcrumb-link title"><slot name="title"></slot></span>
     <span class="breadcrumb-divider">/</span>
     <span class="breadcrumb-link"><slot name="no"></slot></span>
   </nav>
@@ -12,30 +12,37 @@ export default {
 };
 </script>
 
-<style lang="postcss">
+<style scoped lang="postcss">
 .breadcrumb-wrapper {
+  display: inline-flex;
+  align-items: center;
+
   /*hack for overflow ellipsis*/
-  display: inline-block;
   white-space: nowrap;
   overflow: hidden;
   /*text-overflow: ellipsis;*/
 
   /*hack for ellipsis color*/
   color: rgba(21, 81, 166, 0.5);
+
+  /* hack for i18n alignment */
+  line-height: 1.15;
+
 }
 
 .breadcrumb-link {
-  /*hack for alignment after overflow: hidden*/
-  vertical-align: top;
-
+  /* vertical-align: baseline; */
   display: inline-block;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
   max-width: calc(100% - 3em);
 
   & a {
     color: #1e50a2;
+  }
+
+  &.title {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
   }
 }
 
@@ -50,6 +57,5 @@ export default {
   color: #95989A;
   padding: 0em .5rem;
 }
-
 
 </style>
