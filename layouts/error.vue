a3fcd4509cf63ff452f3662aabc8ec726c412734
OrcaXS Suen <sun16281394@gmail.com>
2018-01-21 00:11:27 +0800

    Add akkarin to error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,24 +1,62 @@
 <template>
   <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occured</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
+    <div v-if="error.statusCode === 404">
+      <img
+        src="~/assets/images/transparent_akkarin.jpg"
+        alt="Akkarin not found image"
+      >
+      <p class="img-link">(Credit: <a href="https://www.pixiv.net/member_illust.php?mode=medium&illust_id=22876424">はらぺこ@仕事につきROM専</a>)</p>
+      <h1>404 Page not found</h1>
+    </div>
+    <div v-else>
+      <h1>An error occured</h1>
+      <!-- <pre>{{ JSON.Stringify(error) }}</pre> -->
+      <pre>{{ error.statusCode }}</pre>
+    </div>
+    <pre>{{ error.message }}</pre>
+    <!-- <nuxt-link to="/">Home page</nuxt-link> -->
   </div>
 </template>
 
 <script>
 export default {
-  props: ['error'],
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+  head() {
+    return {
+      title: 'Error | Animeloop',
+    };
+  },
+  computed: {
+    errorStrings() { return JSON.stringify(this.error) },
+  },
 };
 </script>
 
 <style scoped>
 .container {
   font-family: sans-serif;
-  padding-top: 10%;
   text-align: center;
+  /* height: 100vw; */
 }
+
+.img-link {
+  & a {
+    color: royalblue;
+    text-decoration: underline;
+  }
+  margin-bottom: 1em;
+}
+
 h1 {
   font-size: 20px;
+}
+
+img {
+  margin-bottom: 1em;
 }
 </style>
