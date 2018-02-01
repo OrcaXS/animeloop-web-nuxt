2e574dc753f7b1b5a4db5764ba77e9379d659aea
OrcaXS Suen <sun16281394@gmail.com>
2018-02-01 16:52:54 +0800

    Redirect /random to /loop/:id

--- pages/random.vue
+++ pages/random.vue
@@ -1,51 +1,15 @@
-<template>
-  <LoopPage page-type="random" />
-</template>
-
 <script>
-import LoopPage from '~/components/Loop/';
-
 export default {
   name: 'PageRandom',
-  components: {
-    LoopPage,
-  },
 
-  head() {
-    return {
-      title: `${this.loop.episode.no} | ${this.i18nTitle} | Random | Animeloop`,
-    };
-  },
-
-  async fetch({ store, error }) {
+  async fetch({ store, error, redirect }) {
     try {
       await store.dispatch('fetchRandomLoop');
     } catch (err) {
       error({ statusCode: 404, message: 'API returned Error' });
     }
-  },
-
-  computed: {
-    i18nTitle() {
-      switch (this.currentLocale) {
-        case 'ja':
-          return this.loop.series.title_japanese;
-        case 'zh':
-          return this.loop.series.title;
-        case 'en':
-          return this.loop.series.title_english;
-        default:
-          return this.loop.series.title_english;
-      }
-    },
-
-    currentLocale() {
-      return this.$store.state.i18n.locale;
-    },
-
-    loop() {
-      return this.$store.state.loop.loops[this.$store.state.loop.randomPageLoopid];
-    },
+    const randomLoopID = store.state.loop.randomPageLoopid;
+    redirect(302, `/loop/${randomLoopID}`);
   },
 };
 </script>
