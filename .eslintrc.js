818892b6f27b9a4d23ad95e6658e05f155c2224f
OrcaXS Suen <sun16281394@gmail.com>
2018-04-02 00:06:41 +0800

    Add configuration for vue-styleguidist

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   // required to lint *.vue files
   plugins: [
-    // 'html'
+    'jsdoc',
   ],
   // check if imports actually resolve
   settings: {
@@ -50,6 +50,20 @@ module.exports = {
     // allow optionalDependencies
     'import/no-extraneous-dependencies': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+    // jsdoc
+    'jsdoc/check-param-names': 1,
+    'jsdoc/check-tag-names': 1,
+    'jsdoc/check-types': 1,
+    'jsdoc/newline-after-description': 1,
+    'jsdoc/require-description-complete-sentence': 1,
+    'jsdoc/require-hyphen-before-param-description': 1,
+    'jsdoc/require-param': 1,
+    'jsdoc/require-param-description': 1,
+    'jsdoc/require-param-name': 1,
+    'jsdoc/require-param-type': 1,
+    'jsdoc/require-returns-description': 1,
+    'jsdoc/require-returns-type': 1,
   }
 }
---
+++ styleguide.config.js
@@ -0,0 +1,63 @@
+const resolve = require('path').resolve; // eslint-disable-line
+const rules = require('vue-webpack-loaders');
+
+module.exports = {
+  // components() {
+  //   return ['components#<{(||)}>#*.vue', 'pages#<{(||)}>#*.vue'];
+  // },
+  sections: [
+    {
+      name: 'Components',
+      sections: [
+        {
+          name: 'Common',
+          components: 'components/Common/*.vue',
+          description: 'Common Components.',
+        },
+        {
+          name: 'Loop',
+          components: 'components/Loop/*.vue',
+          description: 'Components for Loop Page.'
+        },
+        {
+          name: 'Navbar',
+          components: 'components/Navbar/*.vue',
+          description: 'Navbar Components.',
+        },
+        {
+          name: 'Series',
+          components: 'components/Series/*.vue',
+          description: 'Components for Series/Episodes Page.',
+        },
+        {
+          name: 'Loading',
+          components: 'components/Loading.vue',
+          description: 'Nuxt Loading Progress Bar.',
+        },
+      ],
+    },
+    {
+      name: 'Pages',
+      components: 'pages/**/*.vue',
+    },
+  ],
+  // components: '@(components|pages)#<{(||)}>#*.vue',
+  webpackConfig: {
+    resolve: {
+      modules: [resolve(__dirname, 'lib'), 'node_modules'],
+      extensions: ['.js', '.vue'],
+      alias: {
+        '~': __dirname,
+        '@': __dirname,
+        // your aliases go here.
+      },
+    },
+
+    module: {
+      rules,
+    },
+  },
+  showUsage: true,
+  vuex: 'store/index.js',
+
+};
